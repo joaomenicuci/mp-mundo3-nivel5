38003db8e073319cf88b711fd71da88f8634af6b
João Menicuci <101670764+joaomenicuci@users.noreply.github.com>
2024-06-13 08:59:37 -0300

    Add files via upload

---
+++ CadastroServer/src/cadastroserver/controller/exceptions/NonexistentEntityException.java
@@ -0,0 +1,13 @@
+/**
+ * @author Joao_
+ */
+package cadastroserver.controller.exceptions;
+
+public class NonexistentEntityException extends Exception {
+    public NonexistentEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public NonexistentEntityException(String message) {
+        super(message);
+    }
+}
